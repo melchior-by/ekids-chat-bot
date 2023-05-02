ed5e38013839d2bf041f8f8d02592ed3376c46fb
Artsiom Yurkevich <melchior.by@gmail.com>
2023-05-02 20:00:55 +0200

    add lesson 7

--- src/main/java/by/ekids/BirthdayGreeter.java
+++ src/main/java/by/ekids/BirthdayGreeter.java
@@ -1,2 +1,26 @@
-package by.ekids;public class BirthdayGreeter {
+package by.ekids;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class BirthdayGreeter {
+    private static String greatsSimple[] = {
+            "С днём рождения, %s! Пусть в твоей жизни будет много ярких красок, приятных сюрпризов и незабываемых моментов, которые запомнятся на всю жизнь.",
+            "Сегодня твой день, %s! Желаю тебе, чтобы каждый новый день был наполнен новыми возможностями и приятными открытиями.",
+            "%s, поздравляю с днём рождения! Желаю тебе ощущать себя счастливым и довольным своей жизнью, независимо от того, какие испытания и препятствия могут возникнуть на твоём пути.",
+            "С днём рождения, %! Желаю тебе счастья и радости, любви и душевного спокойствия, успехов и процветания во всех твоих начинаниях.",
+            "Поздравляю с днём рождения, %s! Желаю тебе быть всегда здоровым и энергичным, достигать всех своих целей и мечтаний, находить в жизни много прекрасных моментов и наслаждаться каждым днём."
+    };
+    public static void run(String AIName) {
+        Scanner input = new Scanner(System.in);
+        System.out.println(AIName + ": введите имя поздравляемого");
+        String personName = input.nextLine();
+        Random random = new Random();
+        Boolean aiEngineVariant = random.nextBoolean();
+        if (true) {
+            int guess = random.nextInt(greatsSimple.length + 1);
+            System.out.printf(greatsSimple[guess], personName);
+        }
+
+    }
 }
--- src/main/java/by/ekids/Main.java
+++ src/main/java/by/ekids/Main.java
@@ -1,5 +1,42 @@
-package by.ekids;public class Main {
+package by.ekids;
+
+import java.util.Scanner;
+
+public class Main {
+    private static String[] riddleAnswers = {"человек", "Человек", "ЧЕЛОВЕК"};
+    private static String[] possibleOperations = {"1. Написать поздравление с Днём Рождения",
+            "2. Покинуть диалог"};
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner inputFromKeyboard = new Scanner(System.in);
+
+        System.out.println("Ваше имя ?");
+        String name = inputFromKeyboard.nextLine();
+        System.out.println("Здравствуйте, " + name + " с вами общается искусственный интелект МАВ");
+
+        System.out.println("Для доступа к возможностям ИИ отгадайте загадку:");
+        System.out.println("Что утром ходит на 4 ногах, днем - на 2, а вечером на 3 ?");
+        String answer = inputFromKeyboard.nextLine();
+        Boolean answeredWell = false;
+        for (String possibleAnswer: riddleAnswers) {
+            if (possibleAnswer.equals(answer)) {
+                answeredWell = true;
+            }
+        }
+        if(!answeredWell) {
+            System.out.println("Внезапно к вам в комнату телепортируется Сфинкс и съедает вас");
+        } else {
+            System.out.println("МАВ: Вы ответили верно и можете продолжить диалог");
+        }
+
+        //написать поздравление с др
+        System.out.println("МАВ: Варианты взаимодействия со мной:");
+        for (String possibleOperation: possibleOperations) {
+            System.out.println(possibleOperation);
+        }
+        int numberOfOperation = inputFromKeyboard.nextInt();
+        switch (numberOfOperation){
+            case 2 -> System.out.println("МАВ: всего доброго!");
+            case 1 -> BirthdayGreeter.run("МАВ");
+        }
     }
 }
